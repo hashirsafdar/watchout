0af0f0a5c449a01e9f7a5d9ef7e1c929ca459975
sfn <safefatnoob@gmail.com>
2016-11-13 11:40:31 +0500

    test

--- core/src/sfn/watchout/MenuScreen.java
+++ core/src/sfn/watchout/MenuScreen.java
@@ -14,7 +14,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
